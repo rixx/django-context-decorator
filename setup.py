3be3727b7f3244236e11f3a9ee82d4249f822ab9
Tobias Kunze <r@rixx.de>
2019-04-13 12:55:48 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
-
+import sys
+from os import path
 from setuptools import setup, find_packages
 
+CURRENT_PYTHON = sys.version_info[:2]
+REQUIRED_PYTHON = (3, 6)
+if CURRENT_PYTHON < REQUIRED_PYTHON:
+    sys.stderr.write("""
+==========================
+Unsupported Python version
+==========================
+This version of pretix requires Python {}.{}, but you're trying to
+install it on Python {}.{}.
+""".format(*(REQUIRED_PYTHON + CURRENT_PYTHON)))
+    sys.exit(1)
+
 # Get the long description from the relevant file
-try:
-    with open(path.join(here, '../README.rst'), encoding='utf-8') as f:
-        long_description = f.read()
-except:  # noqa
-    long_description = ''
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='django_context_decorator',
-    version='1.0.0',
+    version='1.0.0.post1',
     author='Tobias Kunze',
     author_email='r@rixx.de',
     license='Apache License 2.0',
@@ -26,6 +37,7 @@ setup(
     ],
     description='Conference organisation: CfPs, scheduling, much more',
     long_description=long_description,
+    python_requires='>=3.6',
     keywords='django context templates',
     packages=find_packages(include=['django_context_decorator']),
     url='https://github.com/rixx/django_context_decorator',
